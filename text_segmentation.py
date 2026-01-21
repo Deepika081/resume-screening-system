686d14f1ad0d3b91830e0c24e429cdd58a1d72f7
Deepika081 <deepika.yadav.diviti@gmail.com>
2026-01-21 20:30:03 +0530

    Fully implemented TSHD Algorithm

--- text_segmentation.py
+++ text_segmentation.py
@@ -1,11 +1,31 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk import PorterStemmer
+from nltk import PorterStemmer,bigrams
 import regex as re
+
+def stem_dict(dictionary):
+    stemmed_dict = {}
+    stemmer = PorterStemmer()
+    for key,value in dictionary.items():
+        if len(key.split())>1:
+            l = key.split()
+            b = [stemmer.stem(i) for i in l]
+            c = ' '.join(b)
+            stem_key = c
+        else:
+            stem_key = stemmer.stem(key)
+        stemmed_dict[stem_key] = value
+    return stemmed_dict
 
 # Preprocessing
 
 def remove_bullets(text):
     cleaned_text = re.sub(r'^[\-\•\*]|\d+\.\s|^[a-zA-Z]$$\s|^[ivx]+\.\s', '', text, flags=re.MULTILINE)
+    return cleaned_text
+
+def remove_punctuations(text):
+    punctuations = """,.:;()[]{}"'•–—/"""
+    pattern = str.maketrans('', '',punctuations)
+    cleaned_text = (text.translate(pattern)).replace('&','and')
     return cleaned_text
 
 text = """
@@ -15,7 +35,7 @@ example@email.com
 Edgar Springs, MO
 Summary
 Competent software engineer, able to work effectively in a fast-paced, agile environment, and passionate about developing software architecture for primarily web applications.
-Experience
+Work Experience
 Software Engineer
 BlueTech Software • Los Angeles, California
 July 2019 - Present
@@ -40,46 +60,131 @@ Continued to develop specialty skills related to his expertise in mobile POS and
 Developed and continuously improved an internal knowledge base of best practices for software development
 Skills
 C, Java, Sdlc, Software Development, Linux, C#, Communication Skills
-Education
+Academic Background
 Bachelor's Degree in Computer Science
 Zirkel College • Los Angeles, California
 """
-new_text = remove_bullets(text)
-l = [i.lower() for i in new_text.split('\n') if i!= '']
+
+stemmer = PorterStemmer()
+
+
+text_without_bullets = remove_bullets(text)
+text_without_punctuations = remove_punctuations(text_without_bullets)
+l = [i.lower() for i in text_without_punctuations.split('\n') if i!= '']
+#print(l)
 new_list = []
 for index,line in enumerate(l):
-    new_list.append((index,line))
+    new_list.append((index,' '.join(stemmer.stem(i) for i in line.split())))
 #print(new_list)
 
 cue_phrases = {
-'personal info':['contact details', 'personal information'],
-'summary':['professional summary', 'career objective','profile summary'],
-'education':['academic background', 'academic qualifications', 'educational qualifications'],
-'experience': ['work experience', 'professional experience', 'employment history'],
-'skills':['technical skills', 'core skills','skill set'],
-'projects': ['academic projects', 'key projects'],
-'certifications': ['professional certifications', 'technical certifications'],
-'languages': ['language proficiency', 'languages known'],
-'interests': ['hobbies and interests', 'interests and activities']
+  "academic background": "education",
+  "academic qualifications": "education",
+  "educational qualifications": "education",
+  "work experience": "experience",
+  "professional experience": "experience",
+  "employment history": "experience",
+  "technical skills": "skills",
+  "core skills": "skills",
+  "skill set": "skills",
+  "professional summary": "summary",
+  "career objective": "summary",
+  "profile summary": "summary",
+  "contact details": "personal_info",
+  "personal information": "personal_info",
+  "academic projects": "projects",
+  "key projects": "projects",
+  "professional certifications": "certifications",
+  "technical certifications": "certifications",
+  "language proficiency": "languages",
+  "languages known": "languages",
+  "hobbies and interests": "interests",
+  "interests and activities": "interests"
 }
-# list_cue_phrase = [i for i in initial_cue_phrases.split('\n')]
-# cue_phrase = [i.split(',') for i in list_cue_phrase]
-
-# # print(cue_phrase)
-# stemming = PorterStemmer()
-# stemmed_list = stemming.stem(initial_cue_phrases)
-
-# print(stemmed_list)
-
 cue_words = {
-    'education': ['education'],
-    'experience': ['experience'],
-    'skills': ['skills'],
-    'summary': ['summary', 'objective'],
-    'projects': ['projects'],
-    'certifications': ['certifications'],
-    'languages': ['languages'],
-    'interests': ['interests', 'hobbies'],
-    'personal info': ['contact'],
-    'others': ['volunteer', 'publications']
+  "education": "education",
+  "experience": "experience",
+  "skills": "skills",
+  "summary": "summary",
+  "objective": "summary",
+  "projects": "projects",
+  "certifications": "certifications",
+  "languages": "languages",
+  "interests": "interests",
+  "hobbies": "interests",
+  "contact": "personal_info",
+  "volunteer": "others",
+  "publications": "others"
 }
+
+stemmed_cue_phrase = stem_dict(cue_phrases)
+stemmed_cue_words = stem_dict(cue_words)
+
+
+# Scan-1
+
+bi_gram = []
+
+for i in new_list:
+    line = remove_punctuations(i[1])
+    #tokenize and stemming
+    stemmed_line = [stemmer.stem(i) for i in line.split()]
+    # print(stemmed_line)
+    # creating bigram
+    bi_gram_line = list(bigrams(stemmed_line))
+    bi_gram.extend(bi_gram_line)
+
+final_bigram = [' '.join(i) for i in bi_gram]
+heading_document = {}
+temp_list = []
+for i in final_bigram:
+    if i in stemmed_cue_phrase.keys():
+        final_list = []
+        value = stemmed_cue_phrase[i]
+        final_list.append(value)
+        tup = [j for j in new_list if i in j]
+        final_list.append(tup)
+        temp_list.append(final_list)
+
+for i in temp_list:
+    heading_document[i[0]] = i[1][0][0]
+
+#print(heading_document)
+
+# Scan-2
+
+for i in new_list:
+    line = i[1].split()
+    if len(line)<4:
+        for j in line:
+            if j in stemmed_cue_words.keys() and stemmed_cue_words[j] not in heading_document.keys():
+                heading_document[stemmed_cue_words[j]] = i[0]
+
+ordered_heading_document = {
+    k:v for k,v in sorted(heading_document.items(), key=lambda item:item[1])
+}
+
+print(ordered_heading_document)
+
+segmented_resume = {}
+indexes = [v for k,v in ordered_heading_document.items()]
+print(indexes)
+list_of_hd = list(ordered_heading_document.items())
+
+for i in range(len(list_of_hd)):
+    start = list_of_hd[i][1]
+    end = list_of_hd[i+1][1] if (i<len(list_of_hd)-1) else len(new_list)
+    print(start,end)
+    segment = [j[1] for j in new_list if (start<j[0] and j[0]<end)]
+    segmented_resume[list_of_hd[i][0]] = segment
+# for key,index in zip(list(ordered_heading_document.items())):
+#     end = index[i+1] if (i<len(indexes)-1) else len(new_list)
+#     # if (i<len(indexes)-1):   
+#     #    end = indexes[i+1]
+#     # else:
+#     #    end = len(new_list)
+#     start = indexes[i]
+#     print(start,end)
+#     segment = [j[1] for j in new_list if (start<j[0] and j[0]<end)]
+#     segmented_resume[key] = segment
+print(segmented_resume)
